89fc5725308aa4b74e59b4f423453eb3043cbbd8
GanbarovEmin <eminganbarov@gmail.com>
2026-05-11 08:27:29 +0400

    release: prepare Streamly v1.0.07

--- Tests/CineFlowDependencyTests/ReleasePackagingTests.swift
+++ Tests/CineFlowDependencyTests/ReleasePackagingTests.swift
@@ -142,5 +142,6 @@ final class ReleasePackagingTests: XCTestCase {
         XCTAssertTrue(workflow.contains("actions/deploy-pages"))
         XCTAssertTrue(workflow.contains("SPARKLE_PRIVATE_KEY"))
         XCTAssertTrue(workflow.contains("--ed-key-file"))
+        XCTAssertFalse(workflow.contains("env.SPARKLE_PRIVATE_KEY != ''"))
     }
 }
